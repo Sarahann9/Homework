9f1808cd61f355a7c67ecc60750f3f7e5c251601
Sarahann9 <sarah.leflore@gmail.com>
2023-11-16 15:58:25 -0800

    Commit

---
+++ src/project/Homework.java
@@ -0,0 +1,11 @@
+package project;
+
+public class Homework{
+    public static void main(String[] args) {
+        System.out.println("First Name: Sample");
+        System.out.println("Last Name: Person");
+        System.out.println("Address: 123 Maple Lane");
+        System.out.println("City: New York");
+        System.out.println("Zip Code: 84432");
+    }
+}
